23f0801918225584e39fe193482739995e1e333b
Sydney Kusumoputro <sydneyk1@yahoo.com>
2013-06-19 15:42:59 -0700

    Amittai's roof success Sydney Amittai

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -2,6 +2,7 @@ package org.teachingkidsprogramming.recipes;
 
 import java.awt.Color;
 
+import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 public class Houses
@@ -17,16 +18,34 @@ public class Houses
   }
   private static void drawHouse(int height)
   {
-    Tortoise.setPenColor(Color.gray.lightGray);
+    Tortoise.setPenColor(colorChooser());
     Tortoise.setSpeed(10);
     Tortoise.hide();
     Tortoise.move(height);
+    //flatRoof();
+    //AmittaisRoof;
+    Tortoise.turn(45);
+    //move 10
+    Tortoise.move(10);
+    //turn 90 right
     Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(90);
+    //move 10
+    Tortoise.move(10);
+    //turn 60
+    Tortoise.turn(45);
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
     Tortoise.turn(-90);
+  }
+  private static void flatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+  }
+  private static Color colorChooser()
+  {
+    return Colors.Grays.LightGray;
   }
 }
