d1345a334cac56027900aa3581f132829c316a99
Shivam Shah <mathwizard98@gmail.com>
2013-06-19 15:23:23 -0700

    IDK-Mihir/Rishit

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class Houses
 {
   public static void main(String[] args)
@@ -7,10 +9,12 @@ public class Houses
     //   Make the tortoise move as fast as possible --#11
     //   Have the tortoise start at 200 pixels in on the X axis --#14
     //   The current height is 40 --#1.2
+    Tortoise.move(height);
     //   drawHouse (recipe below) --#9
     //   ------------- Recipe for drawHouse --#9
     //   Change the color of the line the tortoise draws to lightGray --#15
     //   Move the tortoise the height of a house --#1.1
+    int height = 500;
     //   Turn the tortoise 90 degrees to the right --#2
     //   Move the tortoise 30 pixels --#3
     //   Turn the tortoise 90 degrees to the right --#4
