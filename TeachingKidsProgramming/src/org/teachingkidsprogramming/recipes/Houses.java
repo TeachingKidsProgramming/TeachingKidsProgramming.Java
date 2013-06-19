5bc253d0241b4ce8198a169ae7b6c4799caddc3f
Peter Duong <19duong96@gmail.com>
2013-06-19 15:48:17 -0700

    houses M phuoc hailey

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.awt.Color;
+
 import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
@@ -10,22 +12,53 @@ public class Houses
     Tortoise.setSpeed(10);
     Tortoise.setX(200);
     int height = 40;
-    drawHouse(height);
-    drawHouse(120);
-    drawHouse(90);
-    drawHouse(20);
+    drawHouse(height, Colors.Grays.LightGray);
+    drawHouse(120, Colors.Grays.LightGray);
+    drawHouse(90, Colors.Grays.LightGray);
+    drawHouse(20, Colors.Grays.LightGray);
   }
-  private static void drawHouse(int height)
+  private static void drawHouse(int height, Color color)
   {
-    //   Change the color of the line the tortoise draws to lightGray --#15
-    Tortoise.setPenColor(Colors.Grays.LightGray);
+    Tortoise.setPenColor(color);
     Tortoise.move(height);
+    // flatRoof();
+    //amittasisRoof();
+    //MavericksRoof
+    //turn Left 90
+    Tortoise.turn(-90);
+    //move 10
+    Tortoise.move(10);
+    //turn right 90+45
+    Tortoise.turn(90 + 45);
+    //move 50
+    Tortoise.move(50);
+    //turn 90
     Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(90);
+    // move 50
+    Tortoise.move(50);
+    // turn 90 + 45
+    Tortoise.turn(90 + 45);
+    // move 10 
+    Tortoise.move(10);
+    // turn 90 left
+    Tortoise.turn(-90);
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
     Tortoise.turn(-90);
+  }
+  private static void amittasisRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(10);
+    Tortoise.turn(90);
+    Tortoise.move(10);
+    Tortoise.turn(45);
+  }
+  private static void flatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
   }
 }
