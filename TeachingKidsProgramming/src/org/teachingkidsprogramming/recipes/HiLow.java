34f8b27e0acf0998461c8eee48fca36d9a8e11db
WaRP TuX <javascriptnut@gmail.com>
2013-06-20 14:45:19 -0700

    Completed Sukkah Mihir Amittai numGame

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
@@ -3,31 +3,36 @@ package org.teachingkidsprogramming.recipes;
 import org.teachingextensions.logo.utils.Sounds;
 import org.teachingextensions.windows.MessageBox;
 
+import com.spun.util.NumberUtils;
+
 public class HiLow
 {
   public static void main(String[] args)
   {
-    //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
-    //    Do the following 8 times --#9
-    int guess = MessageBox.askForNumericalInput("Please guess a number. You have 8 tries.");
-    int correctNumber = 20;
-    if (guess == correctNumber)
+    for (int i = 1; i <= 8; i++)
     {
-      Sounds.playBeep();
-      MessageBox.showMessage("YOU WIN :D");
+      //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
+      //    Do the following 8 times --#9
+      int guess = MessageBox.askForNumericalInput("Please guess a number. You have 8 tries.");
+      int correctNumber = NumberUtils.getRandomInt(1, 100);
+      if (guess == correctNumber)
+      {
+        Sounds.playBeep();
+        MessageBox.showMessage("YOU WINzip :D");
+        //     and exit --#10
+        System.exit(0);
+      }
+      else if (guess > correctNumber)
+      {
+        MessageBox.showMessage("You is tooooooo mumbo-jumbo!!!");
+      }
+      else if (guess < correctNumber)
+      {
+        MessageBox.showMessage("You is toooooo teeny-weeny!!");
+      }
+      //    If after 8 times they haven't guessed correctly then --#12
+      //     Tell them they've lost the game --#11
     }
-    //     and exit --#10
-    else if (guess > correctNumber)
-    {
-      MessageBox.showMessage("You is tooooooo mumbo-jumbo!!!");
-    }
-    //     Otherwise, if the guess is too low --#8
-    //     Tell the end user that it is too low --#7
-    else if (guess < correctNumber)
-    {
-      MessageBox.showMessage("You is toooooo teeny-weeny!!");
-    }
-    //    If after 8 times they haven't guessed correctly then --#12
-    //     Tell them they've lost the game --#11
+    MessageBox.showMessage("Yu loozez! Sukkah!!!");
   }
 }
