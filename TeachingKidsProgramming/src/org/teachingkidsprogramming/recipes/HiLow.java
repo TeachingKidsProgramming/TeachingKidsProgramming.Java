5db6544c5996520a5c56b95c34439d8b1a29ff56
Mihir Shah <mihirkrishi@sbcglobal.net>
2013-06-20 23:12:42 -0700

    even better-no green stuff-Mihir

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
@@ -13,8 +13,6 @@ public class HiLow
     int tries = MessageBox.askForNumericalInput("How many tries do you want?");
     for (int i = 1; i <= tries; i++)
     {
-      //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
-      //    Do the following 8 times --#9
       int guess = MessageBox.askForNumericalInput("Please guess a number between 1 and 100. You have "
           + (tries + 1 - i) + " tries.");
       if (guess == correctNumber)
@@ -22,7 +20,6 @@ public class HiLow
         Sounds.playBeep();
         MessageBox.showMessage("Your answer was " + correctNumber + "\n\nYOU WINzip :D\n\nYou took " + i
             + " tries.");
-        //     and exit --#10
         System.exit(0);
       }
       else if (guess > correctNumber)
@@ -33,8 +30,6 @@ public class HiLow
       {
         MessageBox.showMessage("You is toooooo teeny-weeny!!");
       }
-      //    If after 8 times they haven't guessed correctly then --#12
-      //     Tell them they've lost the game --#11
     }
     MessageBox.showMessage("Yu loozez! Sukkah!!! \n\nDa ansah was " + correctNumber);
   }
