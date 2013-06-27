39eb55d3ae7b2059176c4e64e860753c34912a4b
Amittai Groot <javascriptnut@gmail.com>
2013-06-27 15:18:31 -0700

    FinalExamTwo Setup

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/finalExam/ExamTriangleShellTwo.java
@@ -0,0 +1,31 @@
+package org.teachingkidsprogramming.recipes.finalExam;
+
+public class ExamTriangleShellTwo
+{
+  private static int length;
+  public static void main(String[] args)
+  {
+    //    Show the tortoise 
+    //    Make the tortoise move as fast as possible 
+    //    Set the size of a shell to a number that the user supplies HINT: Use a Message Box
+    //    If the size of a shell is less than 300 
+    //         Tell the user that the size of the shell is too small to be seen AND...
+    //         Ask the user to enter a updated size of shell - HINT: Use a Message Box
+    //    Otherwise, Do the following the "shell size" number of times
+    //          Change the color of the line the tortoise draws to a random color 
+    //          Set the value of the large length to the size of the shell divided by 150
+    //          Subtract the value of the large length from the current length
+    //      If the value of the large length is less than zero
+    //      Set the value of the length to the length plus 3
+    //         Otherwise, Call the drawTriangle method (recipe below)      
+    //          Set the value of the number of rotations to 6
+    //          Turn the tortoise 360 times the number of rotations, then divide that by the size of the shell 
+    //    Repeat 
+  }
+  //------------- Recipe for drawTriangle
+  //      Do the following 2.75 times 
+  //          Move the tortoise the current length 
+  //          Turn the tortoise 1/3.25 of 360 degrees 
+  //    Repeat 
+  //------------- End of drawTriangle recipe
+}
