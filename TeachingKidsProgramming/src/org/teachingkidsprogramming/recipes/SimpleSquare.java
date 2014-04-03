7c5ac8e41b3958de9a38d05e6fff09d6206b937d
Llewellyn Falco <isidore@setgame.com>
2014-04-02 21:21:45 -0700

    added VirtualProctor.setName with Lynn
    cool!

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,10 +1,15 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.utils.VirtualProctor;
+
 public class SimpleSquare
 {
   public static void main(String[] args) throws Exception
   {
-    //  Show the tortoise --#1
+    VirtualProctor.internals.resetName();
+    Tortoise.show();
+    Tortoise.move(50);
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
     //      Change the color of the line the tortoise draws to "blue" --#4
