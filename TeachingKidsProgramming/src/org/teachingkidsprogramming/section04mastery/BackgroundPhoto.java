90d82bb16e5529fa03ad06585d09a439e8f6f39c
Lynn Langit <lynnlangit@live.com>
2014-12-31 22:38:53 -0800

    write new recipe BackgroundPhoto w/Samantha

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
@@ -0,0 +1,22 @@
+package org.teachingkidsprogramming.section04mastery;
+
+public class BackgroundPhoto
+{
+  public static void main(String[] args)
+  {
+    //  Make the tortoise go as fast as possible --#3
+    //  Set the current picture to this url "http://img2.timeinc.net/ew/dynamic/imgs/101110/sonny-cher_240.jpg" --#7.1
+    //  Set the background image to the current picture--#7.2
+    //  Set the current side to 2 pixels long --#4.1
+    //  Do the following 75 times --#2
+    //     Set the current pen color to crimson --#8.1
+    //     Use the current pen color for the line the tortoise draws --#8.2
+    //     Increase the length of a side by 1 pixel --#5
+    //     Move the tortoise the length of the current side --#4.2
+    //     Set the X position of the tortoise to 555 pixels --#4.3
+    //     Set the Y position of the tortoise to 65 pixels --#4.4
+    //     Turn the tortoise 1/3rd of 360 degrees to the right --#1
+    //     Turn the tortoise 1 more degree --#6
+    //  Repeat
+  }
+}
