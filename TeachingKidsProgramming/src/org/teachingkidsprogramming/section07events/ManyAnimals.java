8dedcea7418bbcbc032956d23113e7b7f0f3be2a
Lynn Langit <lynnlangit@live.com>
2015-01-09 17:53:52 -0800

    updated English for ManyAnimals

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section07events/ManyAnimals.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07events/ManyAnimals.java
@@ -12,41 +12,47 @@ public class ManyAnimals implements MouseLeftClickListener
   {
     showSomeTurtles();
   }
-  //create an ArrayList of Turtle Objects
+  //create container for Turtles HINT: Use ArrayList
   public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
-  //create a window for many turtles
+  //create a window for many turtles HINT: Use MultiTurtlePanel
   public MultiTurtlePanel  mt      = new MultiTurtlePanel();
   private void showSomeTurtles()
   {
+    //show the panel
     mt.showPanel();
-    //Have SimpleBubble listen for when the left mouse button is clicked in your program window
+    //listen for left mouse button click
     //mt.addMouseLeftClickListener(this);
+    //set the size to 100
     int size = 100;
-    //add three turtles
+    //add three turtles HINT: Use a For loop
     for (int i = 1; i <= 3; i++)
     {
       //create a turtle
       Turtle turtle = new Turtle();
-      //add the turtles to the turtles
+      //add the turtles to the container for turtles
       turtles.add(turtle);
     }
-    //add all the turtles to the window
+    //add all turtles to the window HINT: Use a FOR loop
     for (Turtle turtle : turtles)
     {
-      //must call addTurtle BEFORE calling other methods 
-      //add the first turtle to the windows
+      //NOTE: must call addTurtle BEFORE calling other methods 
+      //add all turtles to the window
       mt.addTurtle(turtle);
     }
-    //set the X, Y - teleport
+    //teleport all turtles on the window HINT: Use a FOR loop and ZERO
     for (int i = 0; i < 3; i++)
     {
+      //set the X position to i*100 + 350 
       turtles.get(i).setX(i * 100 + 350);
+      //set the Y position to i*100 + 100 
       turtles.get(i).setY(i * 100 + 100);
     }
+    //set some values for all turtles HINT: Use a FOR loop
     for (Turtle turtle : turtles)
     {
-      //have the first Turtle draw a star
+      //set the speed of all turtle's to 7
       turtle.setSpeed(7);
+      //have every turtle draw a star of the current size
       turtle.drawStar(size);
     }
   }
