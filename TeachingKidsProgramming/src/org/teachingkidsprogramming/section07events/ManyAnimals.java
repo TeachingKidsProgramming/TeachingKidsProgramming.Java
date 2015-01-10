110745aeefda9416939756fe43611b227ebb6294
Lynn Langit <lynnlangit@live.com>
2015-01-09 17:26:08 -0800

    worked on Many Animals w/Jim

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section07events/ManyAnimals.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07events/ManyAnimals.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.section07events;
 
+import java.util.ArrayList;
+
 import org.teachingextensions.logo.MultiTurtlePanel;
 import org.teachingextensions.logo.Turtle;
 import org.teachingextensions.windows.MouseLeftClickListener;
@@ -10,30 +12,61 @@ public class ManyAnimals implements MouseLeftClickListener
   {
     showSomeTurtles();
   }
+  //create an ArrayList of Turtle Objects
+  public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
+  //create a window for many turtles
+  public MultiTurtlePanel  mt      = new MultiTurtlePanel();
   private void showSomeTurtles()
   {
-    MultiTurtlePanel mt = new MultiTurtlePanel();
-    Turtle t1 = new Turtle();
-    mt.addTurtle(t1);
-    t1.setX(100);
-    t1.setY(300);
-    Turtle t2 = new Turtle();
-    mt.addTurtle(t2);
-    t1.setX(300);
-    t1.setY(100);
     mt.showPanel();
+    //Have SimpleBubble listen for when the left mouse button is clicked in your program window
+    //mt.addMouseLeftClickListener(this);
+    int size = 100;
+    //add three turtles
+    for (int i = 1; i <= 3; i++)
+    {
+      //create a turtle
+      Turtle turtle = new Turtle();
+      //add the turtles to the turtles
+      turtles.add(turtle);
+    }
+    //add all the turtles to the window
+    for (Turtle turtle : turtles)
+    {
+      //must call addTurtle BEFORE calling other methods 
+      //add the first turtle to the windows
+      mt.addTurtle(turtle);
+    }
+    //set the X, Y - teleport
+    for (int i = 0; i < 3; i++)
+    {
+      turtles.get(i).setX(i * 100 + 350);
+      turtles.get(i).setY(i * 100 + 100);
+    }
+    for (Turtle turtle : turtles)
+    {
+      //have the first Turtle draw a star
+      turtle.setSpeed(7);
+      turtle.drawStar(size);
+    }
   }
   @Override
+  //threading error - Virtual Proctor contention
   public void onLeftMouseClick(int x, int y)
   {
-    // createTurtles (recipe below) --#8
-    //------------- Recipe for createTurtles --#8
-    // Remove previous Tortoise from your program window --#9
-    // programWindow.removePaintable();
-    // Set the count of Turtles to a random number between 10 and 50 --#2.5
-    // Create a tortoise at 200, 300 --#2.1
-    // Add the circle to your program window --#2.4
-    //------------- End of createTurtles recipe --#8
+    int size = 200;
+    //create a new Turtle
+    Turtle turtle = new Turtle();
+    //add another turtle to the turtles
+    turtles.add(turtle);
+    // the turtle to the window
+    mt.addTurtle(turtle);
+    turtle.show();
+    // teleport the new turtle to the x and y of the mouse click
+    turtle.setX(x);
+    turtle.setY(y);
+    // have this turtle draw a star
+    turtle.drawStar(size);
   }
   public static void main(String[] args)
   {
