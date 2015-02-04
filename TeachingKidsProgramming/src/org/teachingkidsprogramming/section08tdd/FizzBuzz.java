faefb6f498b870de42a4144c44a383ffb7a753d1
Lynn Langit <lynnlangit@live.com>
2015-02-04 13:36:41 -0800

    Updated FizzBuzz - Koan and TDD

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section08tdd/FizzBuzz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section08tdd/FizzBuzz.java
@@ -1,13 +1,14 @@
 package org.teachingkidsprogramming.section08tdd;
 
-public class FizzBuzzTDD
+public class FizzBuzz
 {
-  //for the numbers being tested, print out either that number, or, 
+  //for the whole numbers from 1 to 100, print either that number, or, 
   //if that number is evenly divisible by 3, then print the word 'Fizz', 
   //if that number is evenly divisible by 5, then print the word 'Buzz', 
   //if that number is evenly divisible by either 3 or 5, then print the word 'FizzBuzz'
   //
-  //write tests using the Assert object via the TDD style
+  //NOTE: this is a kata (higher level instructions)
+  //part of the exercise is to translate into line-by-line English, THEN Java
   //
   //for more complete directions see this page
   //https://www.penflip.com/lynnlangit/tkp-lesson-plans/blob/master/course09.txt
