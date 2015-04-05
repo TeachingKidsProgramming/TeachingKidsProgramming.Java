2800369ae5ef7fa6dec50fe4984a8937e9140d75
lynnlangit <lynnlangit@live.com>
2015-04-04 23:18:18 -0700

    added DoubleLoop recipe

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/DoubleLoop.java
@@ -0,0 +1,27 @@
+package org.teachingkidsprogramming.section01forloops;
+
+
+public class DoubleLoop
+{
+  public static void main(String[] args)
+  {
+    //    Show the tortoise --#1
+    //    Set the tortoise speed to 9 --#4
+    //    Add Crimson to the Color Wheel --#6.1
+    //    Add Red to the Color Wheel --#6.2
+    //    Add Fire Brick to the Color Wheel --#6.3
+    //    Do the following 6 times --#3.1
+    //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#7
+    //         Move the tortoise 4 times the current line number you are drawing --#5
+    //         Turn the tortoise 1/6 of 360 degrees to the left --#2
+	// 
+	//    	    Do the following 15 times --#3.1 (HINT: Use a new 'j' not 'i'!) --#8.1
+    //         		Set the pen width of the line the tortoise draws to 17 --#11
+    //         		Move the tortoise 8 times the current line number you are drawing --#9
+    //         		Turn the tortoise 1/5 of 360 degrees to the right --#10
+    //    		Repeat --#8.2 
+	//  
+	//    Hide the Tortoise --#12
+    //    Repeat --#3.2
+  }
+}
