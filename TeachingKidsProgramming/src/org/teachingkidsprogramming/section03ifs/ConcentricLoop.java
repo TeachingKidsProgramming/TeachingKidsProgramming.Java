1647e8264a815cf3fea4757227ce15d155339fca
lynnlangit <lynnlangit@live.com>
2015-04-10 16:17:43 -0700

    new recipe w/Matt and Samantha

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
@@ -0,0 +1,22 @@
+package org.teachingkidsprogramming.section03ifs;
+ 
+public class ConcentricLoop
+{
+  public static void main(String[] args)
+  {
+    
+    //  Set the tortoise x position to 150 --#4.1
+    //  Set the tortoise y position to 200 --#4.2
+    //  Set the tortoise speed to 10  --#4.3
+    //  Do the following 360 times --#3.1
+    //      Move the tortoise 3 pixels --#1
+    //      Turn the tortoise 1 pixel --#2
+    //      If i is divisible by 20 --(HINT: Use mod operator '%') --#7
+    //          Do the following 360 times --#8.1
+    //              Move the tortoise 1 pixel  --#5  
+    //              Turn the tortoise 1 pixel --#6    
+    //              Change the width of the line that the tortoise draws to 1/100th of the current line --#9      
+    //          Repeat --#8.2
+    //   Repeat --#3.2
+  }
+}
