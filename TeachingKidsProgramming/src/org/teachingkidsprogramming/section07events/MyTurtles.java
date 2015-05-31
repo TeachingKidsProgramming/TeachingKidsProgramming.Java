f4d31ed0f4b617908e3dd587db6d926c6dd1626b
lynnlangit <lynnlangit@live.com>
2015-05-31 11:11:17 -0700

    updated MyTurtles w/Jim

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section07events/MyTurtles.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07events/MyTurtles.java
@@ -1,10 +1,15 @@
 package org.teachingkidsprogramming.section07events;
 
+import java.util.ArrayList;
+
+import org.teachingextensions.logo.MultiTurtleWindow;
+import org.teachingextensions.logo.Turtle;
+
 public class MyTurtles
 {
   //Uncomment the two lines of code below to create a container and a window for your turtles --#1
-  //public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
-  //public MultiTurtleWindow mtw     = new MultiTurtleWindow();
+  public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
+  public MultiTurtleWindow mtw     = new MultiTurtleWindow();
   //
   public MyTurtles()
   {
@@ -17,23 +22,20 @@ public class MyTurtles
     //Create a turtle instance  --#2.1
     //Add your turtles to your turtle container --#2.2
     //Repeat --#4.2
-    //
     //Create a variable to count the number of turtles in the container --#3.1
-    //Show the number of turtles in the container in a Message Box --#3.2
+    //Uncomment to Show the number of turtles in the container on the window --#3.2
     //
-    //Use a FOREACH loop to add your turtles to your window --#4.1
-    //Add your turtles to your window --#4.3
-    //Set the turtle speed to 9 --#5.1
-    //Get the turtles and have them draw a triangle with a side of 50 pixels --#5.2
-    //Repeat --#4.2
-    //
+    //Use a FOREACH loop to add your turtles to your window --#5.1
+    //Add and Show your turtles on your window --#5.3
+    //Repeat --#5.2
     //Use a FOR loop to teleport all of your turtles around your window  --#6.1
     //Get your turtle's current position and then set the X position to i*100 + 350 --#6.3
     //Get your turtle's current position and then set the Y position to i*100 + 100 --#6.4
     //Repeat --#6.2
-    //
     //Use a FOREACH loop to set values for all of your turtles --#7.1
-    //Have each turtle draw a star with a side that is 100 pixels --#7.3
+    //Set the pen width to 10 pixels -- #7.3
+    //Set the turtle speed to 9 --#7.4
+    //Have each turtle draw a star with a side that is 100 pixels --#7.5
     //Repeat --#7.2
   }
   //
