cdce97109e072cc99e0a4d646561defba677e90f
lynnlangit <lynnlangit@live.com>
2015-07-13 14:14:25 -0700

    moved to new virtual proctor w/Jim

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/TriangleShell.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/TriangleShell.java
@@ -3,14 +3,14 @@ package org.teachingkidsprogramming.section02methods;
 @SuppressWarnings("unused")
 public class TriangleShell
 {
-  private static int length = 1;
+  private static int length;
   public static void main(String[] args)
   {
     // Show the tortoise --#1
-    // Make the tortoise go as fast as possible --#8
+    // Make the tortoise go as fast as possible --#6
     // Do the following 60 times --#7.1
     //      Change the pen color of the line the tortoise draws to a random color --#9
-    //      Increase the current length of the side by 4 pixels --#6
+    //      Increase the current length of the side by 4 pixels --#8
     //      drawTriangle (recipe below) --#5.1
     //
     //      ------------- Recipe for drawTriangle --#5.2
