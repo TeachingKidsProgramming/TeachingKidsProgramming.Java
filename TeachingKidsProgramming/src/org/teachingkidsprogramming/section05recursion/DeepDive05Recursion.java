74cda1a2300b8726c87d9bb64f9409f0b3a49ba7
Jim Counts <jcounts18@gmail.com>
2015-07-14 09:47:57 -0700

    Optimized imports

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section05recursion/DeepDive05Recursion.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section05recursion/DeepDive05Recursion.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.teachingextensions.logo.PenColors;
 import org.teachingextensions.logo.Tortoise;
