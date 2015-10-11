39c0553440eab5b47d253e1c176c65b79ff8af17
lynnlangit <lynnlangit@live.com>
2015-10-11 11:18:56 -0700

    added demo recipe QuickTortoise

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section00demos/QuickTortoise.java
@@ -0,0 +1,9 @@
+package org.teachingkidsprogramming.section00demos;
+
+public class QuickTortoise
+{
+  public static void main(String[] args) throws Exception
+  {
+    // Use the Tortoise to draw a Tortoise!
+  }
+}
