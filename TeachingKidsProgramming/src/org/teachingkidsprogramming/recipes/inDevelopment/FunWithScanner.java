5e1f27b10d16423308ff037e8dccf012aba46096
lynnlangit <lynnlangit@live.com>
2015-10-31 16:56:41 -0700

    added to Java Scanner recipe w/@Samanthalangit

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/inDevelopment/FunWithScanner.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/inDevelopment/FunWithScanner.java
@@ -1,31 +1,37 @@
 package org.teachingkidsprogramming.recipes.inDevelopment;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class FunWithScanner
 {
   private static Scanner scanner;
-  private static Scanner scanner2;
   public static void main(String[] args)
   {
-    scanner = new Scanner(System.in);
-    scanner.useDelimiter("\\n");
-    System.out.print("Enter an index: ");
-    int index = scanner.nextInt();
-    System.out.print("Enter a sentence: ");
-    String sentence = scanner.next();
-    System.out.println("\nYour sentence: " + sentence);
-    System.out.println("Your index: " + index);
+    makeAString();
   }
   public static void makeAString()
   {
-    String input = "1 fish 2 fish red fish blue fish";
-    scanner2 = new Scanner(input);
-    Scanner s = scanner2.useDelimiter("\\s*fish\\s*");
-    System.out.println(s.nextInt());
-    System.out.println(s.nextInt());
-    System.out.println(s.next());
-    System.out.println(s.next());
+    String input = "1 fish 2 fish red fish blue fish, ";
+    scanner = new Scanner(input);
+    System.err.println("\nIt is said that there are: " + input);
+    Scanner s = scanner.useDelimiter("\\s*fish\\s*");
+    System.out.println("So:       " + s.nextInt() + " and " + s.nextInt());
+    System.out.println("And:      " + s.next() + " and " + s.next() + '\n');
+    for (String unit : new Iterable<String>()
+    {
+      private String units = "black fish,blue fish,old fish,new fish";
+      @Override
+      public Iterator<String> iterator()
+      {
+        Scanner scanner = new Scanner(units);
+        scanner.useDelimiter(",");
+        return scanner;
+      }
+    })
+    {
+      System.out.println("And also: " + unit);
+    }
     s.close();
   }
 }
