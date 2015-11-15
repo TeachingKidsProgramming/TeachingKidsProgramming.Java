a98f3eb199c3c1382518010dd7846b932c233c26
lynnlangit <lynnlangit@live.com>
2015-11-15 11:12:20 -0800

    new recipe TurtleObjects

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/TurtleObjects.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/TurtleObjects.java
@@ -1,59 +1,41 @@
 package org.teachingkidsprogramming.section07objects;
 
 import org.teachingextensions.WindowUtils.MultiTurtleWindow;
-import org.teachingextensions.logo.Turtle;
-//
-//NOTE: This recipe is in development and will be updated as it goes through our testing process
-//
 
 public class TurtleObjects
 {
-  //Uncomment the line of code below to create a window for your turtles --#1
   public MultiTurtleWindow mtw = new MultiTurtleWindow();
-  //
-  public static void main(String[] args)
-  {
-    new TurtleObjects();
-  }
   public TurtleObjects()
   {
     showSomeTurtles();
   }
-  //
+  public static void main(String[] args)
+  {
+    new TurtleObjects();
+  }
   private void showSomeTurtles()
   {
-    makeSpeedyTurtle();
-    makeSlowTurtle();
-    makeCrazyTurtle();
+    //  Call the makeSpeedyTurtle method --#2
+    //  Call the makeSlowTurtle method --#4
+    //  Call the makeCrazyTurtle method --#6
   }
-  //Create this method and body --#2
-  //Teacher to point out the mtw.addAndShowTurtle method
-  //Teacher to ask 'what are the steps of the drawTriangle method'?
-  private void makeSpeedyTurtle()
-  {
-    Turtle speedyTurtle = new Turtle();
-    speedyTurtle.setSpeed(10);
-    mtw.addAndShowTurtle(speedyTurtle);
-    speedyTurtle.drawTriangle(100);
-  }
-  //Create this method and body --#3
-  //Teacher to ask why this turtle is slower than the first one
-  //Teacher to ask why the two turtles start at the same spot
-  private void makeSlowTurtle()
-  {
-    Turtle slowTurtle = new Turtle();
-    mtw.addAndShowTurtle(slowTurtle);
-    slowTurtle.drawTriangle(-50);
-  }
-  //Create this method and body --#4
-  //Teacher to ask why is this turtle faster than the second one
-  //Teacher to ask why does this turtle 'start' in a different location
-  //Teacher to ask why does this turtle show up AFTER the other two HINT: mtw.addTurtle method
-  //Teacher to ask what does the drawLightning method do?
-  private void makeCrazyTurtle()
-  {
-    Turtle crazyTurtle = new Turtle();
-    mtw.addTurtle(crazyTurtle);
-    crazyTurtle.drawLightning(55);
-  }
+  //
+  //    Create the makeSpeedyTurtle method --#1.0
+  //        Create a new speedyTurtle instance  
+  //        Set the speed of your speedyTurtle to the fastest possible    
+  //        Add your speedyTurtle to your MultiTurtleWindow
+  //        Have your speedyTurtle draw a triangle with 100 pixel sides
+  //    End of makeSpeedyTurtle method --#1.1 
+  //
+  //    Create the makeSlowTurtle method --#3.0
+  //        Create a new slowTurtle instance 
+  //        Add your slowTurtle to your MultiTurtleWindow
+  //        Have your slowTurtle draw a upside down triangle with 50 pixel sides
+  //    End of makeSlowTurtle method --#3.1
+  //
+  //    Create the makeCrazyTurtle method --#5.0
+  //        Create a new crazyTurtle instance  
+  //        Add your crazyTurtle to your MultiTurtleWindow
+  //        Have your crazyTurtle draw a 55 pixel long lightning bolt
+  //    End of makeCrazyTurtle method --#5.1
 }
