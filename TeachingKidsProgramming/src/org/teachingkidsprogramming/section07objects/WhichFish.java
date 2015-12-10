18f32b9b4be96d58c6489774befcf11dafd6e997
lynnlangit <lynnlangit@live.com>
2015-12-09 21:21:19 -0800

    new recipe WhichFish with @samanthalangit

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section07objects/WhichFish.java
@@ -0,0 +1,47 @@
+package org.teachingkidsprogramming.section07objects;
+
+//*************This Lesson is In Development*****************************//
+public class WhichFish
+{
+  public static void main(String[] args)
+  {
+    //  UNCOMMENT to create a variable called 'numberOfFish' to hold the result of the question "How many fishes?" -- #INFO
+    //  Use the FancyMessageBox with a title of "Are fishes like wishes?" -- #INFO
+    //      int numberOfFish = FancyMessageBox.askForNumericalInput("How many fishes?", "Are Fishes Like Wishes?"); --#1
+    //  Create recipe makeAFishyDecision using the numberOfFish (recipe below) --#2
+    //----------recipe for makeAFishyDecision with the numberOfFish---------------------/
+    //  Use a switch...case on the numberOfFish --#3
+    //      When the numberOfFish is -1 --#4
+    //      UNCOMMENT to create a string of this image
+    //          String image = "../TeachingKidsProgramming.Source.Java/src/main/resources/icons/thumb-up.png";
+    //      Create a new ImageIcon from your image
+    //      UNCOMMENT to Show a message with the fancy message box this text, this title and this icon...
+    //          FancyMessageBox.showMesage("Had a Fish", "Not hungry anymore...", icon);
+    //      End
+    //      When the numberOfFish is 0  --#4
+    //      UNCOMMENT to create a string of this image
+    //          String image0 = "../TeachingKidsProgramming.Source.Java/src/main/resources/icons/information.png";
+    //      Create a new ImageIcon from your image
+    //      Show a message with the fancy message box this text, this title and this icon...
+    //      End
+    //      When the numberOfFish is 1 --#5   
+    //      UNCOMMENT to create a string of this image
+    //          String image1 = "../TeachingKidsProgramming.Source.Java/src/main/resources/icons/star.png";
+    //      Create a new ImageIcon from your image
+    //      Show a message with the fancy message box this text, this title and this icon...
+    //      End
+    //      When the numberOfFish is 2 --#6    
+    //      UNCOMMENT to create a string of this image
+    //          String image2 = "../TeachingKidsProgramming.Source.Java/src/main/resources/icons/github.png";
+    //      Create a new ImageIcon from your image
+    //      Show a message with the fancy message box this text, this title and this icon...
+    //      End
+    //      Otherwise  --#7
+    //      UNCOMMENT to create a string of this image
+    //          String image4 = "../TeachingKidsProgramming.Source.Java/src/main/resources/icons/hint.png";
+    //      Create a new ImageIcon from your image
+    //      Show a message with the fancy message box this text, this title and this icon...
+    //      End
+    //--------end of recipe for makeAFishyDecision-------------------------------------/
+  }
+}
