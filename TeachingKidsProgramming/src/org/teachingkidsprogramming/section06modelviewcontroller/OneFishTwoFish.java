db62b4b0aac7c3a0252736175589b6ce3ecfe3a5
lynnlangit <lynnlangit@live.com>
2015-12-13 18:59:19 -0800

    new recipe OneFishTwoFish

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/OneFishTwoFish.java
@@ -0,0 +1,34 @@
+package org.teachingkidsprogramming.section06modelviewcontroller;
+
+//*************This Lesson is In Development*****************************//
+@SuppressWarnings("unused")
+public class OneFishTwoFish
+{
+  // Create a Scanner to make a string
+  public static void main(String[] args)
+  {
+    makeAString();
+  }
+  public static void makeAString()
+  {
+    final String input = "1 fish 2 fish red fish blue fish,black fish,blue fish,old fish,new fish ";
+    // Use your scanner with your input
+    // Display a new line and "We have: " and the input and then a new line
+    // Now, tell a story with a new Scanner instance
+  }
+  private static void tellAStory(final String input)
+  {
+    // Use 'fish' as the base text w/your scanner [story] HINT: useDelimeter
+    // Get the next number to use in your scanner [story] HINT: nextInt
+    // Get the next value to use in your scanner [story] HINT: next
+    // Iterate over each fish [string] HINT: Interable
+    // Create a new string iterator
+    // Create a new scanner for your input
+    // Use a comma to separate the strings in your story HINT: useDelimeter
+    // Return the result
+    {
+      //  Display "And then: " and the fish in the console
+    }
+    System.out.println("");
+  }
+}
