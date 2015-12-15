0280116af50cf6405dd5b40b1690c7b9714dde3e
lynnlangit <lynnlangit@live.com>
2015-12-14 21:43:15 -0800

    added SquareKata

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/SquareKata.java
@@ -0,0 +1,11 @@
+package org.teachingkidsprogramming.section09final;
+
+public class SquareKata
+{
+  public static void main(String[] args)
+  {
+    //    Draw a square 
+    //    HINT: Write each step in English FIRST, then translate to Java one line at a time
+    //    TIP: Be sure to run after each line of Java to make sure it works as expected
+  }
+}
---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/SquareKataTDD.java
@@ -0,0 +1,22 @@
+package org.teachingkidsprogramming.section09final;
+
+//--------------------This recipe is in progress-------------------------------//
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.teachingkidsprogramming.recipes.completed.section09final.FizzBuzzTDD;
+
+public class SquareKataTDD
+{
+  //    Draw a square 
+  //    HINT: Write each step in English FIRST, then translate to Java one line at a time
+  //    TIP: Be sure to run after each line of Java to make sure it works as expected
+  //    Write tests using the Assert object via the TDD style shown below
+  //    TIP: Fix this test to make it pass to get started
+  @Test
+  public void test1Returns1()
+  {
+    String result = FizzBuzzTDD.convert(1);
+    assertEquals("2", result);
+  }
+}
