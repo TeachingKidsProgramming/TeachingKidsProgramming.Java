0419d40d2673411946e74825a2b08a73f45f3479
jychuah <jychuah@gmail.com>
2016-01-08 14:25:29 -0600

    Got one leg of square

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
@@ -1,15 +1,20 @@
 package org.teachingkidsprogramming.section01forloops;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class SimpleSquare
 {
   public static void main(String[] args) throws Exception
   {
     //  Show the tortoise --#1
+    Tortoise.show();
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
     //      Change the pen color of the line the tortoise draws to blue --#4
     //      Move the tortoise 50 pixels --#2
+    Tortoise.move(50);
     //      Turn the tortoise to the right (90 degrees) --#3
+    Tortoise.turn(90);
     //  Repeat --#5.2
     //
     //  (Optional): Sign your work using the Virtual Proctor 
