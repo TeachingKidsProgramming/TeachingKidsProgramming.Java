bd2749e6c42c24478a0f45de3ab774743546969b
jychuah <jychuah@gmail.com>
2016-01-08 14:27:03 -0600

    Draws 4 legs now

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
@@ -1,6 +1,7 @@
 package org.teachingkidsprogramming.section01forloops;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SimpleSquare
 {
@@ -8,16 +9,20 @@ public class SimpleSquare
   {
     //  Show the tortoise --#1
     Tortoise.show();
-    //  Make the tortoise move as fast as possible --#6
-    //  Do the following 4 times --#5.1
-    //      Change the pen color of the line the tortoise draws to blue --#4
-    //      Move the tortoise 50 pixels --#2
-    Tortoise.move(50);
-    //      Turn the tortoise to the right (90 degrees) --#3
-    Tortoise.turn(90);
-    //  Repeat --#5.2
-    //
-    //  (Optional): Sign your work using the Virtual Proctor 
-    //  See your work at http://virtualproctor.tkpjava.org
+    for (int i = 0; i < 4; i++)
+    {
+      //  Make the tortoise move as fast as possible --#6
+      //  Do the following 4 times --#5.1
+      //      Change the pen color of the line the tortoise draws to blue --#4
+      Tortoise.setPenColor(PenColors.Blues.Blue);
+      //      Move the tortoise 50 pixels --#2
+      Tortoise.move(50);
+      //      Turn the tortoise to the right (90 degrees) --#3
+      Tortoise.turn(90);
+      //  Repeat --#5.2
+      //
+      //  (Optional): Sign your work using the Virtual Proctor 
+      //  See your work at http://virtualproctor.tkpjava.org
+    }
   }
 }
