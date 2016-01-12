eb0c2fd374938f554af2a764f9bed2269574e341
Joon Yee Chuah <joonyee.chuah@austinisd.org>
2016-01-12 11:27:52 -0600

    Showed and moved tortoise

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
@@ -1,7 +1,6 @@
 package org.teachingkidsprogramming.section01forloops;
 
 import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SimpleSquare
 {
@@ -9,21 +8,16 @@ public class SimpleSquare
   {
     //  Show the tortoise --#1
     Tortoise.show();
-    for (int i = 0; i < 4; i++)
-    {
-      //  Make the tortoise move as fast as possible --#6
-      Tortoise.setSpeed(10);
-      //  Do the following 4 times --#5.1
-      //      Change the pen color of the line the tortoise draws to blue --#4
-      Tortoise.setPenColor(PenColors.Blues.Blue);
-      //      Move the tortoise 50 pixels --#2
-      Tortoise.move(50);
-      //      Turn the tortoise to the right (90 degrees) --#3
-      Tortoise.turn(90);
-      //  Repeat --#5.2
-      //
-      //  (Optional): Sign your work using the Virtual Proctor 
-      //  See your work at http://virtualproctor.tkpjava.org
-    }
+    //  Make the tortoise move as fast as possible --#6
+    //  Do the following 4 times --#5.1
+    //      Change the pen color of the line the tortoise draws to blue --#4
+    //      Move the tortoise 50 pixels --#2
+    Tortoise.move(50);
+    //      Turn the tortoise to the right (90 degrees) --#3
+    Tortoise.turn(90);
+    //  Repeat --#5.2
+    //
+    //  (Optional): Sign your work using the Virtual Proctor 
+    //  See your work at http://virtualproctor.tkpjava.org
   }
 }
