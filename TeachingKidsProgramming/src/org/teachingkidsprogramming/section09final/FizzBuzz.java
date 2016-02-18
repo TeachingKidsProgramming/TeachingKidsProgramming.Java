53bd2ac621844d42b77ce72a221e9033dce2dbdc
lynnlangit <lynnlangit@live.com>
2016-02-17 21:51:34 -0800

    addd new recipe FizzBuzzGoldenMaster w/ @samanthalangit

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/FizzBuzz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.teachingkidsprogramming.section09final;
 
 public class FizzBuzz
 {
-  //    For the whole numbers from 1 to 100, print either that number, or, 
+  //   For the whole numbers from 1 to 100, print either that number, or, 
   //    If that number is evenly divisible by 3, then print the word 'Fizz', 
   //    If that number is evenly divisible by 5, then print the word 'Buzz', 
   //    If that number is evenly divisible by either 3 or 5, then print the word 'FizzBuzz'
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
@@ -2,7 +2,7 @@ package org.teachingkidsprogramming.section09final;
 
 public class FizzBuzzTDD
 {
-  //    For the numbers being tested, print out either that number, or, 
+  //   For the numbers being tested, print out either that number, or, 
   //    If that number is evenly divisible by 3, then print the word 'Fizz', 
   //    If that number is evenly divisible by 5, then print the word 'Buzz', 
   //    If that number is evenly divisible by either 3 or 5, then print the word 'FizzBuzz'
