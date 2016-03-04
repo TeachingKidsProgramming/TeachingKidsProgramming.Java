e73f072fc4c94dfcf3315e690a198a280a55b6d7
lynnlangit <lynnlangit@live.com>
2016-03-03 20:00:28 -0800

    new recipe MakeALogo w/ @dcoopersmith

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section00demos/MakeALogo.java
@@ -0,0 +1,19 @@
+package org.teachingkidsprogramming.section00demos;
+
+public class MakeALogo
+{
+  public static int YValue = 300;
+  public static int XValue = 100;
+  public static void main(String[] args)
+  {
+    //  Show the Tortoise --#1
+    //  Make the Tortoise draw a line as fast as possible --#2
+    //  Setup the Color Wheel --#3 HINT: Use the TKPLogo object
+    //  Draw a TKP 'T' --#4
+    //  Draw a left bracket --#8
+    //  Draw a TKP 'K' --#5
+    //  Draw a right bracket --#9
+    //  Draw an outer TKP 'P' --#6
+    //  Call the make_Inner_P() method --#7
+  }
+}
