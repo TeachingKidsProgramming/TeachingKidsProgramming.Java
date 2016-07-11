8fd4f09b71971b2d1bd88dd5fd615006334f1bde
lynnlangit <lynnlangit@live.com>
2016-07-10 19:16:58 -0700

    added PyramidsOfGiza starter recipe

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/inDevelopment/PyramidsOfGiza.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/inDevelopment/PyramidsOfGiza.java
@@ -1,23 +1,18 @@
 package org.teachingkidsprogramming.recipes.inDevelopment;
 
 import org.teachingextensions.logo.Tortoise;
-//  NOTE: this might need to be broken into several different recipes??
-//  e.g. PyramidsOfGiza01, PyramidsOfGiza02, PyramidsOfGiza03, etc...
-//  This is a very simple/basic recipe introducing passing multiple values 
-//  into methods and reinforcing basic refactoring.
-//  This can be completed in several 'passes' or 'steps of refactoring' as follows:
 //
-//  1. entire recipe written in linear fashion with lots of repeated code
+//  1. This recipe written in linear fashion with lots of repeated code
 //  2.  a) turnAndMove() method created 
 //      b) every 2 linear turn() and move() calls are replaced by 1 call to new turnAndMove() method
 //  3. Now another pattern emerges, and a 3rd refactoring/abstraction takes place
 //      when the student notices that there are several each of 
-//      turnAndMove(90, 100) & turnAndMove(-90, 100), so 2 new methods are written:
-//      turnAndMove(90, 100) becomes -> moveDown() & turnAndMove(-90, 100) -> moveUp()
+//              turnAndMove(90, 100) & turnAndMove(-90, 100), so 2 new methods are written:
+//              turnAndMove(90, 100) becomes -> moveDown() & turnAndMove(-90, 100) -> moveUp()
 //  4. At this point, students are encouraged to see if there might be any other possible patterns
 //      to refactor
 
-//  Where to add?  In Course 1 or Course 2 - concepts taught: refactoring
+//  Where to add?  In Course 1 or Course 2 - concepts taught: method arguments (variables) and refactoring
 public class PyramidsOfGiza
 {
   public static void main(String[] args) throws Exception
---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/inDevelopment/PyramidsOfGizaStarter.java
@@ -0,0 +1,22 @@
+package org.teachingkidsprogramming.recipes.inDevelopment;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class PyramidsOfGizaStarter
+{
+  public static void main(String[] args) throws Exception
+  {
+    setupTortoise();
+    // USER STORY: Write the code to draw 3 pyramids
+    // Draw a sketch
+    // Write out each step in English
+    // Write the code one line at a time
+    // Run your program to verify that your code works
+    // Remove duplicate code by using the 'extract method' re-factoring
+  }
+  private static void setupTortoise()
+  {
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+  }
+}
