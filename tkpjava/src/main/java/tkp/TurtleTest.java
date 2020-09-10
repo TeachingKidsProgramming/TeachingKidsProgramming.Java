f3bd1405d3a90ec203face7bca7e578f35dbe84b
Lynn Langit <lynnlangit@live.com>
2020-09-10 14:46:00 +0000

    creating Hello Turtle

--- tkpjava/src/main/java/tkp/TurtleTest.java
+++
@@ -1,9 +0,0 @@
-package tkp;
-
-public final class TurtleTest {
-    private TurtleTest() {
-    }
-    public static void main(String[] args) {
-        System.out.println("Hello TKP World!");
-    }
-}
