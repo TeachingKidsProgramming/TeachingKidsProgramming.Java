18312c49d02dd69cf96edd564829a03f9ab5f3d8
Lynn Langit <lynnlangit@live.com>
2020-09-10 22:24:54 +0000

    added first Tortoise test java file

--- TKPJava/src/main/java/org/verify-TKP/AppSrc.java
+++
@@ -1,7 +0,0 @@
-public class AppSrc 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello TKP Source World!" );
-    }
-}
---
+++ TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
@@ -0,0 +1,8 @@
+public class HelloTKPTortoise 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello TKP World!" );
+        //Tortoise.show();
+    }
+}
