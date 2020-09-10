dfe7703e41e5b75b2627fdc3c144824035e689fd
Lynn Langit <lynnlangit@live.com>
2020-09-10 02:40:47 +0000

    basic verification

---
+++ TeachingKidsProgramming/org/teachingkidsprogramming/section00demos/QuickTortoise.java
@@ -0,0 +1,12 @@
+package org.teachingkidsprogramming.section00demos;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class QuickTortoise
+{
+  public static void main(String[] args) throws Exception
+  {
+    Tortoise.show();
+    // Use the Tortoise object to draw a Tortoise!
+  }
+}
