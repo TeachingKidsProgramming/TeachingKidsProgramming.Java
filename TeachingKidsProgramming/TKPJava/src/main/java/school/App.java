9a3f26c9c61305fa8ca7386b8fe65bd9d4339ab5
Lynn Langit <lynnlangit@live.com>
2020-09-10 15:38:14 +0000

    adding hello worlds

---
+++ TeachingKidsProgramming/TKPJava/src/main/java/school/App.java
@@ -0,0 +1,13 @@
+package school;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello TKP World!" );
+    }
+}
---
+++ TeachingKidsProgramming/src/main/java/org/helloverifications/AppSrc.java
@@ -0,0 +1,13 @@
+package school;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppSrc 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello TKP Source World!" );
+    }
+}
