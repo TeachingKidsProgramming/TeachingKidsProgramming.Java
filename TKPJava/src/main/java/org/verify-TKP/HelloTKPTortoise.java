1c2a4703975ff07eb2367eebb3560ad09614b5d0
Lynn Langit <lynnlangit@live.com>
2020-09-13 20:16:08 +0000

    testing MessageBox

--- TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
+++ TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
@@ -1,10 +1,12 @@
 import src.main.java.org.teachingextentions.logo.Tortoise;
+import src.main.java.org.teachingextentions.logo.utils.EventUtils.MessageBox;
 
 public class HelloTKPTortoise 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello TKP Tortoise World!" );
-        Tortoise.show();
+       System.out.println( "Hello TKP Tortoise World!" );
+       MessageBox.showMessage("message");
+       //Tortoise.show();
     }
 }
