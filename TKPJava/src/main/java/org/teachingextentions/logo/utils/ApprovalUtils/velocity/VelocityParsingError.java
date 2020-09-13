cad228ac5d9b2c9bd9efa8215f5b7a22d8b3ce59
Lynn Langit <lynnlangit@live.com>
2020-09-13 16:13:09 +0000

    updating VP Error

--- TKPJava/src/main/java/org/teachingextentions/logo/utils/ApprovalUtils/velocity/VelocityParsingError.java
+++ TKPJava/src/main/java/org/teachingextentions/logo/utils/ApprovalUtils/velocity/VelocityParsingError.java
@@ -1,6 +1,10 @@
 package src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.velocity;
 
+import org.apache.velocity.util.introspection.Info;
 
 public class VelocityParsingError {
+
+	public VelocityParsingError(String string, Info i) {
+	}
 
 }
