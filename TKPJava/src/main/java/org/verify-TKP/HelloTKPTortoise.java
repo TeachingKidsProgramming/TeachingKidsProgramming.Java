9cf71f82ace30ee4e54a87c9dc8b74e2aba0c545
Lynn Langit <lynnlangit@live.com>
2020-09-13 20:23:22 +0000

    test harness for GUI issue

--- TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
+++ TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
@@ -1,12 +1,23 @@
 import src.main.java.org.teachingextentions.logo.Tortoise;
+import src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.general.NumberUtils;
 import src.main.java.org.teachingextentions.logo.utils.EventUtils.MessageBox;
 
 public class HelloTKPTortoise 
 {
     public static void main( String[] args )
     {
+       // First test - does this file work - YES
        System.out.println( "Hello TKP Tortoise World!" );
-       MessageBox.showMessage("message");
-       //Tortoise.show();
+    
+       // Second test - does `teachingextentions` work - YES
+       Integer i = NumberUtils.getRandomInt(1, 100);
+       System.out.println("number "+ (i));
+
+       // Third test - does `MessageBox` work - NO
+       // need diff JAVA version in DOCKERFILE, using 'headless' - can't render GUI
+       // MessageBox.showMessage("message");
+
+       // Fourth Test - does Tortoise window work - NO - can't render GUI
+       // Tortoise.show();
     }
 }
