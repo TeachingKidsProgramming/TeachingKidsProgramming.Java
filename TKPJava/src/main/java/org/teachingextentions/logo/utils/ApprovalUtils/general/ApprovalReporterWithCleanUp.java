d204641b5509daad098d175d2a0e5b7f69397e68
Lynn Langit <lynnlangit@live.com>
2020-09-13 16:04:02 +0000

    updating CleanUp

--- TKPJava/src/main/java/org/teachingextentions/logo/utils/ApprovalUtils/general/ApprovalReporterWithCleanUp.java
+++ TKPJava/src/main/java/org/teachingextentions/logo/utils/ApprovalUtils/general/ApprovalReporterWithCleanUp.java
@@ -3,3 +3,5 @@ package src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.general;
 public interface ApprovalReporterWithCleanUp {
     public void cleanUp(String received, String approved) throws Exception;
 }
+
+
