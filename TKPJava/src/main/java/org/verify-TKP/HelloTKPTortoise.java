f1b93ff3aaeaf673ba4e4b222c419f4b603d7263
Lynn Langit <lynnlangit@live.com>
2020-09-23 20:36:54 +0000

    removing port 5901 in config

--- TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
+++ TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
@@ -1,5 +1,8 @@
 import javax.swing.JOptionPane;
-//import src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.general.NumberUtils;
+
+import src.main.java.org.teachingextentions.logo.Tortoise;
+import src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.general.NumberUtils;
+import src.main.java.org.teachingextentions.logo.utils.EventUtils.MessageBox;
 
 public class HelloTKPTortoise 
 {
@@ -9,25 +12,22 @@ public class HelloTKPTortoise
        System.out.println( "Hello TKP Tortoise World!" );
     
        // TEST 2 - does `teachingextentions` work - YES
-       //Integer i = NumberUtils.getRandomInt(1, 100);
-       //System.out.println("number "+ (i));
+       Integer i = NumberUtils.getRandomInt(1, 100);
+       System.out.println("number "+ (i));
 
        // TEST 3 - does `playSound` work - NO
        // URL to /soundFiles folder is mangled, copied `BrainIsGone.wav` to same dir for testing
        // Sound s = new Sound(TKPSound.BrainIsGone);
        //s.playSound();
        
-       // TEST 4  - does `JOptionPane` ( becomes `MessageBox`) work - NO
-       // need diff JAVA version in DOCKERFILE, using 'headless' - can't render GUI
-       // see - https://stackoverflow.com/questions/5218870/getting-a-headlessexception-no-x11-display-variable-was-set
-       // must configure from Docker also - see http://fabiorehm.com/blog/2014/09/11/running-gui-apps-with-docker/
+       // TEST 4  - does `JOptionPane` ( becomes `MessageBox`) work - YES
        String message = "JOption";
        JOptionPane.showMessageDialog(null, message);
 
        // TKPJava `wrapper` for JOptionPane is MessageBox
-       // MessageBox.showMessage("message");
+       //MessageBox.showMessage("this is the message");
       
        // TEST 5 - does Tortoise window work - NO - can't render GUI
-       // Tortoise.show();
+       //Tortoise.turn(90);
     }
 }
