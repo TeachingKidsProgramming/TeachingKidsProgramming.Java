1718ba8abe2cf9c1375f95c188c1673fcc191ec3
Lynn Langit <lynnlangit@live.com>
2020-09-25 12:29:06 +0000

    reset test file

--- TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
+++ TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
@@ -1,29 +1,26 @@
-import javax.swing.JOptionPane;
-
-import src.main.java.org.teachingextentions.logo.Tortoise;
 import src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.general.NumberUtils;
-import src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.reporters.windows.TortoiseImageDiffReporter;
 
 public class HelloTKPTortoise 
 {
     public static void main( String[] args )
     {
-       // TEST 1 - does this file work - YES
+       // TEST 1 - does this file work
        System.out.println( "Hello TKP Tortoise World!" );
     
-       // TEST 2 - does `teachingextentions` work - YES
+       // TEST 2 - does `teachingextentions` work
        Integer i = NumberUtils.getRandomInt(2, 102);
        System.out.println("number "+ (i));
  
-       // TEST 3  - does `JOptionPane` ( becomes `MessageBox`) work - YES
-       String message = "JOption";
-       JOptionPane.showMessageDialog(null, message);
+       // TEST 3  - does `JOptionPane` ( becomes `MessageBox`) work
+       // String message = "JOption";
+       // JOptionPane.showMessageDialog(null, message);
 
        // TKPJava `wrapper` for JOptionPane is MessageBox
-       //MessageBox.showMessage("this is the message");
+       // MessageBox.showMessage("this is the message");
       
-       // TEST 4 - does Tortoise window work - NO - can't render GUI
-       Tortoise.turn(90);
-       Tortoise.show();
+       // TEST 4 - does Tortoise window work 
+       // Tortoise.show();
+       // Tortoise.move(50);
+       // Tortoise.turn(90);
     }
 }
