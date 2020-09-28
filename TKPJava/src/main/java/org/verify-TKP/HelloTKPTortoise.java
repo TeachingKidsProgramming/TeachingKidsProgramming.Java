39e7bcf1b1a996daba28feaf09c4b5821bfa4cf0
Lynn Langit <lynnlangit@live.com>
2020-09-28 16:17:00 +0000

    update test file

--- TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
+++ TKPJava/src/main/java/org/verify-TKP/HelloTKPTortoise.java
@@ -1,26 +1,33 @@
+import src.main.java.org.teachingextentions.logo.Tortoise;
 import src.main.java.org.teachingextentions.logo.utils.ApprovalUtils.general.NumberUtils;
 
 public class HelloTKPTortoise 
 {
     public static void main( String[] args )
     {
-       // TEST 1 - does this file work
+       // TEST 1 - just print something
        System.out.println( "Hello TKP Tortoise World!" );
     
-       // TEST 2 - does `teachingextentions` work
+       // TEST 2 - use a class.method from  `teachingextentions` library
        Integer i = NumberUtils.getRandomInt(2, 102);
        System.out.println("number "+ (i));
  
-       // TEST 3  - does `JOptionPane` ( becomes `MessageBox`) work
-       // String message = "JOption";
+       // TEST 3  - use a Java primitive `JOptionPane` ( becomes `MessageBox`) 
+       // String message = "TKP JOption";
        // JOptionPane.showMessageDialog(null, message);
 
        // TKPJava `wrapper` for JOptionPane is MessageBox
-       // MessageBox.showMessage("this is the message");
+       // MessageBox.showMessage("TKP message");
       
-       // TEST 4 - does Tortoise window work 
-       // Tortoise.show();
-       // Tortoise.move(50);
-       // Tortoise.turn(90);
+       // TEST 4 - use TKPJava Tortoise window  
+       Tortoise.show();
+       Tortoise.setSpeed(5);
+       for (int j = 0; j < 4; j++) {
+        Tortoise.move(50);
+        Tortoise.turn(90);
+        Tortoise.move(50);
+       }
+       
+    
     }
 }
